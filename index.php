03d5b1f32bc4152f553717e32ef9979461db582e
kucheriavii <kucheriavii@gmail.com>
2017-02-02 21:21:28 +0200

    start project

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<link rel="stylesheet" href="css/main.css">
+	<title>Loft blog</title>
+</head>
+<body>
+	<!--[if lt IE 9]>
+		<p class="chromeframe">It seams that you use old brovser version</p>
+	<![endif-->
+	<div class="wrapper">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, ex! Neque dolorem itaque repudiandae veritatis, sapiente molestiae pariatur maiores ipsa, officiis quo. Exercitationem ullam qui sed, impedit quaerat nesciunt similique!
+		
+		<div class="empty"></div> <!--Заглушка чтобы не wrapper на наползал на footer-->
+	</div>
+
+	<footer class="footer">
+		<div class="fooer__address">
+			c.Ladomeria-city BC "Baikal"
+		</div>
+		<div class="footer__copyright">
+			&copy; Kucheriavii
+		</div>
+	</footer>
+</body>
+</html>
